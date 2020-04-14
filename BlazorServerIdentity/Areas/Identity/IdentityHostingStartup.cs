6c25aea7bced121bb64bd91ee90007bf06f78c8b
Brian Douglass <corp@tps-eoltp.com>
2020-04-14 16:17:31 -0700

    --New Identity tree files

---
+++ BlazorServerIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BlazorServerIdentity.Areas.Identity.Data;
+using BlazorServerIdentity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorServerIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorServerIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlazorServerIdentityContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("BlazorServerIdentityContextConnection")));
+
+                services.AddDefaultIdentity<BlazorServerIdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlazorServerIdentityContext>();
+            });
+        }
+    }
+}
